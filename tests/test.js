b0c2d630f1d31cb94796a0de7a03fde1dc26eb04
Brett Huffman <dicer2000@yahoo.com>
2019-10-14 16:58:23 -0500

    Changes to testing file

--- tests/test.js
+++ tests/test.js
@@ -9,7 +9,7 @@ const request = supertest(app);
 describe('Tests app', function() {
   it('verifies get', function(done) {
     request.get('/').expect(200).end(function(err, result) {
-      test.string(result.text).contains('Congratulations');
+      test.string(result.text).contains('BrettHuffman.com All Rights Reserved');
       test.value(result).hasHeader('content-type', 'text/html; charset=UTF-8');
       done(err);
     });
